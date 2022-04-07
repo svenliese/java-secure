1881889bf6e94b1250303bb7dca22ea7b9dfef8e
svenliese <sven_liese@web.de>
2022-04-07 10:36:17 +0200

    feature: confirm new password

--- src/main/java/de/sl/secure/Main.java
+++ src/main/java/de/sl/secure/Main.java
@@ -12,8 +12,12 @@ public class Main {
     private static final String OPTION_CANCEL = "cancel";
     private static final String OPTION_OK = "ok";
     private static final String OPTION_SAVE = "save";
+    private static final String OPTION_RETRY = "retry";
 
     private static final String PREF_FILENAME = "filename";
+
+    private static final String MSG_ENTER_PASS = "enter password";
+    private static final String MSG_CONFIRM_PASS = "confirm password";
 
     private static SecureFile getFile() {
         final Preferences preferences = Preferences.userNodeForPackage(Main.class);
@@ -56,7 +60,7 @@ public class Main {
             return new SecureFile(file);
         }
 
-        char[] pass = getPass();
+        char[] pass = getPassForDecryption(MSG_ENTER_PASS);
         if(pass.length==0) {
             return null;
         }
@@ -64,13 +68,13 @@ public class Main {
         return new SecureFile(file, new String(pass));
     }
 
-    private static char[] getPass() {
+    private static char[] getPassForDecryption(String title) {
         final String[] options = {OPTION_OK, OPTION_CANCEL};
         final JPasswordField passwordField = new JPasswordField();
         final int what = JOptionPane.showOptionDialog(
             null,
             passwordField,
-            "enter password",
+            title,
             JOptionPane.DEFAULT_OPTION,
             JOptionPane.PLAIN_MESSAGE,
             null,
@@ -81,6 +85,49 @@ public class Main {
             return new char[0];
         }
         return passwordField.getPassword();
+    }
+
+    private static boolean passEqual(char[] first, char[] second) {
+        return new String(first).equals(new String(second));
+    }
+
+    private static boolean retry() {
+        final String[] options = {OPTION_RETRY, OPTION_CANCEL};
+        final int what = JOptionPane.showOptionDialog(
+            null,
+            "passwords not equal !",
+            "error",
+            JOptionPane.DEFAULT_OPTION,
+            JOptionPane.ERROR_MESSAGE,
+            null,
+            options,
+            options[0]
+        );
+        return options[what].equals(OPTION_RETRY);
+    }
+
+    private static char[] getPassForEncryption() {
+
+        char[] first;
+        char[] second;
+        do {
+            first = getPassForDecryption(MSG_ENTER_PASS);
+            if (first.length == 0) {
+                return first;
+            }
+
+            second = getPassForDecryption(MSG_CONFIRM_PASS);
+            if (second.length == 0) {
+                return second;
+            }
+
+            if(passEqual(first, second)) {
+                return first;
+            }
+
+        } while(retry());
+
+        return new char[0];
     }
 
     private static String editFile(SecureFile file) {
@@ -118,7 +165,7 @@ public class Main {
         }
 
         if(!file.isEncrypted()) {
-            final char[] pass = getPass();
+            final char[] pass = getPassForEncryption();
             if (pass.length == 0) {
                 return;
             }
