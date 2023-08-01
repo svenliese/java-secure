5c855c87c10e41dc14005164cf779b461c7df829
svenliese <63915696+svenliese@users.noreply.github.com>
2023-08-01 09:07:58 +0200

    search text

--- src/main/java/de/sl/secure/Editor.java
+++ src/main/java/de/sl/secure/Editor.java
@@ -2,12 +2,16 @@ package de.sl.secure;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import static de.sl.secure.Constants.*;
 
-public class Editor {
+public class Editor implements KeyListener {
 
     private SecureFile file;
+
+    private JEditorPane editor;
 
     public Editor(SecureFile file) {
         this.file = file;
@@ -16,7 +20,8 @@ public class Editor {
     public boolean editFile() {
         final String[] options = {OPTION_SAVE, OPTION_NEW_PASS, OPTION_CANCEL};
 
-        final JEditorPane editor = new JEditorPane();
+        editor = new JEditorPane();
+        editor.addKeyListener(this);
         editor.setText(file.readContent());
 
         final JScrollPane scrollPane = new JScrollPane(editor);
@@ -50,5 +55,55 @@ public class Editor {
         file.saveContent(content);
 
         return true;
+    }
+
+    private String getSearchText() {
+        final String[] options = {OPTION_OK, OPTION_CANCEL};
+
+        JTextField input = new JTextField();
+
+        final int what = JOptionPane.showOptionDialog(
+                editor,
+                input,
+                "search for",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                options,
+                input
+        );
+
+        if(what<0 || options[what].equals(OPTION_CANCEL)) {
+            return null;
+        }
+
+        return input.getText();
+    }
+
+    private void goTo(String searchFor) {
+        final int pos = editor.getText().indexOf(searchFor);
+        if(pos>=0) {
+            editor.setCaretPosition(pos);
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode()==70 && e.isControlDown()) {
+            final String searchFor = getSearchText();
+            if(searchFor!=null && !searchFor.isEmpty()) {
+                goTo(searchFor);
+            }
+            editor.requestFocus();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // not used
     }
 }
